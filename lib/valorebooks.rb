9047d3c1d23885e11bb13762197c1c327255d5a5
Gilbert Pleczynski <gpleczynski@gmail.com>
2014-10-16 11:17:32 -0400

    Initial Commit

--- lib/valorebooks.rb
+++ lib/valorebooks.rb
@@ -2,90 +2,60 @@ require "valorebooks/version"
 require "valorebooks/xml_node"
 
 module Valorebooks
-	class ProductAPI
-		API_URL = "http://prices.valorebooks.com/lookup-multiple-categories"
+  class ProductAPI
+    API_URL = "http://prices.valorebooks.com/lookup-multiple-categories"
 
-	  def get_products(opts = {})
-	  	puts opts.merge!({SiteID: opts[:SiteID] || ENV["VALOREBOOKS_SITE_ID"]})
-	  	puts query_params = opts.map{ |k,v| "#{k}=#{v}"}
-	  	url = "#{API_URL}?#{query_params.join("&")}"
+    def get_products(opts = {})
+      puts opts.merge!({SiteID: opts[:SiteID] || ENV["VALOREBOOKS_SITE_ID"]})
+      puts query_params = opts.map{ |k,v| "#{k}=#{v}"}
+      url = "#{API_URL}?#{query_params.join("&")}"
 
-	  	response = RestClient.get(url)
-	  	Response.new(response)
-		end
-	end
+      response = RestClient.get(url)
+      Response.new(response)
+    end
+  end
 
 
-	class Response
-		def initialize(body)
-			@body = body
-		end
+  class Response
+    def initialize(body)
+      @body = body
+    end
 
-		def xml
-			@xml ||= MultiXml.parse(@body)
-		end
-		alias_method :parse, :xml
+    def xml
+      @xml ||= MultiXml.parse(@body)
+    end
+    alias_method :parse, :xml
 
-		def success?
-			xml["error"].nil?
-		end
-		alias_method :valid?, :success?
+    def success?
+      xml["error"].nil?
+    end
+    alias_method :valid?, :success?
 
-		def to_object
-			@as_object ||= hash_to_ostruct(xml)	
-		end
+    def to_object
+      @as_object ||= hash_to_ostruct(xml) 
+    end
 
-		def to_s
-			@body	
-		end
-		alias_method :body, :to_s
+    def to_s
+      @body 
+    end
+    alias_method :body, :to_s
 
-		private
-		def hash_to_ostruct(object)
-		  return case object
-		  when Hash
-		  	result = {}
-		    object = object.dup
-		    object.each do |key, value|
-		      result[key.downcase] = hash_to_ostruct(value)
-		    end
-		    OpenStruct.new(result)
-		  when Array
-		    object = object.dup
-		    object.map! { |i| hash_to_ostruct(i) }
-		  else
-		    object
-		  end
-		end
-	end
-
+    private
+    def hash_to_ostruct(object)
+      return case object
+      when Hash
+        result = {}
+        object = object.dup
+        object.each do |key, value|
+          result[key.downcase] = hash_to_ostruct(value)
+        end
+        OpenStruct.new(result)
+      when Array
+        object = object.dup
+        object.map! { |i| hash_to_ostruct(i) }
+      else
+        object
+      end
+    end
+  end
 end
-
-
-
-
-
-
-def reap_it(isbn)
-	remove = ["alternate_edition", "link", "comments", "shipping_options"]
-
-	response = Valorebooks::ProductAPI.new.get_products(ProductCode: isbn, Level: "detailed", NumberToShow: 5)
-	product = response.xml["product"]
-
-	unless product["sale_offer"].is_a? Array
-	 	product["sale_offer"] = [product["sale_offer"]]
-	end 
-
-	out = []
-	product["sale_offer"].each do |offer|
-		out << offer.reject{ |k,v| remove.include? k  }
-	end
-	out
-end
-
-
-
-
-
-
-
